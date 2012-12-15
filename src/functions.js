d43c4c40c65621009d6dbdf371f6a94edbfece32
Andrew Gerst <gerst20051@gmail.com>
2012-12-14 19:44:26 -0500

    updated google analytics code

--- src/functions.js
+++ src/functions.js
@@ -218,7 +218,7 @@ function hasModifier(){
 }
 
 (function(){
-	window._gaq=[['_setAccount','UA-36970192-1'],['_trackPageview']];
+	window._gaq=[['_setAccount','UA-37052695-1'],['_trackPageview']];
 	var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;
 	ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';
 	var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);
